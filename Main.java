c9f5cf2aca09cbf15e8bdb8a93177e1b381bef4d
Jonathan <jbarreramartinez100@student.hbuhsd.edu>
2025-02-24 19:07:20 +0000

    done

--- Main.java
+++ Main.java
@@ -11,8 +11,23 @@ public class Main
     String word = "";
     while (!word.equals("STOP"))
     {
-      System.out.println("Implement me!");
+      System.out.println("Please enter words, enter STOP to stop the loop.");
+      word = sc.nextLine();
+      input.add(word);
     }
+    System.out.println("Enter the letter to search for");
+    String worder = sc.nextLine();
+    System.out.println("Search for: " + countSecondInitial(input, worder));
+    ArrayList<String> input2 = new ArrayList<String>();
+    String worder2 = "";
+    while(!worder2.equals("STOP")) {
+      System.out.println("Please enter words again, enter STOP to stop the loop.");
+      worder2 = sc.nextLine();
+      input2.add(worder2);
+    }
+    System.out.println("Enter the word to search for");
+    String target = sc.nextLine();
+    System.out.println("Search for: " + searchSecond(input2, target));
   }
 
   /** Returns the number of Strings in the ArrayList that has the letter
@@ -22,7 +37,13 @@ public class Main
    */
   public static int countSecondInitial(ArrayList<String> arr, String letter)
   {
-    return 0;
+    int counter = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr.get(i).substring(1, 2).equals(letter)) {
+          counter++;
+        }
+    }
+    return counter;
   }
 
   /** Returns the index of the second occurrence of target within the ArrayList
@@ -31,6 +52,17 @@ public class Main
    */
   public static int searchSecond(final ArrayList<String> arr, String target)
   {
+    int counter = 0;
+    int appearance = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        String element = arr.get(i);
+        if(element.equals(target)) {
+            appearance++;
+        }
+        if(appearance > 1) {
+          return i;
+        }
+    }
     return -1;
   }
 }
